cd2e3702a05986b72cf0dcfe154ef73247328f48
Maksym Sobolyev <sobomax@sippysoft.com>
2025-05-03 11:30:31 -0700

    Use python/requirements.txt to install dependencies.
    Simplify pip cache management and make it working.

--- scripts/build/install_depends_wheels.sh
+++ scripts/build/install_depends_wheels.sh
@@ -6,4 +6,4 @@ PYTHON_CMD="${PYTHON_CMD:-"python${PY_VER}"}"
 
 ${PYTHON_CMD} -m ensurepip --upgrade
 ${PYTHON_CMD} -m pip install --upgrade pip
-${PYTHON_CMD} -m pip install --upgrade build setuptools wheel auditwheel numpy
+${PYTHON_CMD} -m pip install --upgrade build setuptools wheel auditwheel
